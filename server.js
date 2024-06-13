19a00cce285a9e2d428ef266318817b5feb09370
Davi Santos <davi.ssantosads@gmail.com>
2024-06-13 08:37:11 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+// Middleware para analisar o corpo de requisições POST
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Disponibilizar arquivos estáticos vindos do diretório 'src'
+app.use(express.static(path.join(__dirname, 'src')));
+
+// Disponibilizar o aquivo 'index.html' como a URL raiz
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/html/index.html'));
+});
+
+// Lidar com logins de usuáios
+app.post('/login', (req, res) => {
+    const { codigo, senha, categoria } = req.body;
+
+    // Credenciais
+    const validCodigo = '123';
+    const validSenha = '123';
+    const validCategoria = 'gerente';
+
+    // Verificação das credenciais
+    if ((codigo === validCodigo) && (senha === validSenha) && (categoria == validCategoria)) {
+        if (categoria == 'gerente'){
+            res.redirect('/html/F_gerenteGUI.html');
+        }
+        res.redirect('/html/F_colaboradorGUI.html');
+    } else {
+        res.send(`
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Invalid Data</title>
+            </head>
+            <body>
+                <script>
+                    alert('Dados inválidos. Por favor, tente novamente.');
+                    window.location.href = '/'; // Redirect back to the main page after the alert
+                </script>
+            </body>
+            </html>
+        `);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
